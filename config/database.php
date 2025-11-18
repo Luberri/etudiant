e1463d2662dd1900100afb1fe1ce36ff6a46017d
Luberri <fandresimahery@gmail.com>
2025-11-18 07:49:45 +0300

    oiyuse

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 Flight::register('db', 'PDO', array(
-    'mysql:host=localhost;port=3306;dbname=gestion_scolarite;charset=utf8',
+    'mysql:host=db;port=3306;dbname=gestion_scolarite;charset=utf8',
     'root',
     'root'
 ), function ($db) {
